a503d80893096f32566e6c191a8cac3124bf1af7
ElCrazyGeek <elcrazygeeksoyyo@gmail.com>
2025-03-13 13:31:39 -0600

    Marcador

--- Assets/Saque.cs
+++ Assets/Saque.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using TMPro; // con esto importamos comandos de TextMeshPro
 
 public class Saque : MonoBehaviour
 { 
+    public  int ScoreP1; //varibles numericas
+
+    public  int ScoreP2;
+    public TextMeshProUGUI textoP1, textoP2; //variables de texto 
+
     Rigidbody2D PelotaRB;//Este es el rigitbody de 
     public float fuerza;
 
@@ -25,8 +31,21 @@ public class Saque : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        playing=false;
+        if(collision.gameObject.CompareTag("GolP1")){
+           playing=false;
         PelotaRB.linearVelocity=Vector2.zero;
         transform.position=Inicio.position;
+            ScoreP1++;
+            textoP1.text="Player 1\n " +ScoreP1.ToString();//cambiamos el texto
+        }
+
+         if(collision.gameObject.CompareTag("GolP2")){
+            playing=false;
+        PelotaRB.linearVelocity=Vector2.zero;
+        transform.position=Inicio.position;
+            ScoreP2++;
+            textoP2.text="Player 2\n " +ScoreP2.ToString();//cambiamos el texto 
+        }
+        
     }
 }
